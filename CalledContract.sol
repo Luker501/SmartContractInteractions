35f110917b710ceac05750b43c396140697af6ec
Luker501 <luker501@hotmail.com>
2018-08-03 22:48:50 +0100

    Added two solidity files
    These are two files that show all of the different ways that two smart contracts can interact via solidity (without assembly)

---
+++ CalledContract.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.4.20;
+
+/*
+ * @title CalledContract
+ * @author Luke Riley (https://www.linkedin.com/in/luke-riley-36484221/)
+ * @notice The purpose of this smart contract is to show all of the different 
+ * ways that two smart contracts can interact.
+ * Therefore the functions in this contract are called by the corresponding CallingContract
+ */
+contract CalledContract {
+  
+    //@param message - a variable that changes depending on what function is called
+   string public message;
+
+    //an event that is emitted whenever the message variable is changed
+   event MessageChange(string message);
+   //an event that is emitted whereever ether can be received
+   event ReceivedFunds(uint value);
+   
+ /*
+  * @notice Instantiates this contract, allowing it to receive ether
+  */
+   constructor () public payable {}
+
+ /*
+  * @notice A simple function that cannot receive ether
+  */  
+  function helloFunction() public {
+      
+     message = "hello there";
+     emit MessageChange(message);
+     return;
+     
+  }
+  
+  /*
+  * @notice A simple function that can receive ether
+  * @param msg.value - The ether the user wants to give to this contract
+  */
+  function callMeFunction() public payable {
+     
+     message = "I was called";
+     emit MessageChange(message);
+     emit ReceivedFunds(msg.value);
+     return;
+     
+  }
+  
+  /*
+  * @notice A getter function that allows the user to check how much ether the contract has
+  */ 
+  function checkEth() public view returns(uint) {
+      
+      return address(this).balance;
+      
+  }
+  
+  /*
+  * @notice The fallback function that is the default function used to recieve ether
+  * note that if you want send and transfer invocations to use the fallback then
+  * only a emit log can be coded in the function.
+  * @param msg.value - The ether the user wants to give to this contract
+  */
+  function () public payable {
+    
+    //uncomment the following two lines if you want to check that send and
+    //transfer invocation do not allow extra functionality in the fallback
+    //function:
+    //message = "just paid via the fallback function";
+    //emit MessageChange(message);
+    emit ReceivedFunds(msg.value);
+    
+  }
+
+}
---
+++ CallingContract.sol
@@ -0,0 +1,124 @@
+pragma solidity ^0.4.20;
+import "browser/CalledContract.sol";
+
+/*
+ * @title CallingContract
+ * @author Luke Riley (https://www.linkedin.com/in/luke-riley-36484221/)
+ * @notice The purpose of this smart contract is to show all of the different 
+ * ways that two smart contracts can interact.
+ */
+contract CallingContract {
+    
+   //@param message is used to show how the delegation invocation works
+   string public message;
+
+  /*
+  * @notice Instantiates a new contract
+  */
+  function DirectInvocationA() public payable {
+      
+      CalledContract newContract = new CalledContract();
+
+  }
+  
+  /*
+  * @notice Loads a contract and calls a function on it
+  * @param addressOfContract - The address of the contract to interact with
+  */
+  function DirectInvocationB(address addressOfContract) public {
+      
+      CalledContract newContract = CalledContract(addressOfContract);
+      newContract.helloFunction();
+    
+  }
+  
+  /*
+  * @notice Calls the fallback function of the contract at the given address
+  * @param addressOfContract - The address of the contract to interact with
+  * @param msg.value - The ether the user wants to give to the contract at addressOfContract
+  */
+  function LowLevelCallInvocationA(address addressOfContract) public payable {
+      
+      //A low level call does not propagate exceptions, 
+      //therefore a require function has been added to propagate an exception
+      //if one is found (when the return is false)
+      require(addressOfContract.call.value(msg.value).gas(200000)() == true);
+      
+  }
+  
+  /*
+  * @notice Attempts to call the hardcoded function given via string ("callMeFunction()"), 
+  * of the contract at the given address. If "callMeFunction()" is not found, then the 
+  * fallback function will run instead
+  * @param addressOfContract - The address of the contract to interact with
+  * @param msg.value - The ether the user wants to give to the contract at addressOfContract
+  */
+  function LowLevelCallInvocationB(address addressOfContract) public payable {
+     
+      //A low level call does not propagate exceptions, 
+      //therefore a require function has been added to propagate an exception
+      //if one is found (when the return is false)
+      require(addressOfContract.call.value(msg.value).gas(600000)(bytes4(keccak256("callMeFunction()"))) == true);
+      
+  }
+
+  /*
+  * @notice Similar to the previous function, but in this case the called function
+  * will run using this function's storage variables. I.e. the message variable changed
+  * by the helloFunction() will be the one in this contract
+  * @param addressOfContract - The address of the contract to interact with
+  */
+  function DelegationInvocation(address addressOfContract) public payable {
+
+      //A delegation does not propagate exceptions, 
+      //therefore a require function has been added to propagate an exception
+      //if one is found (when the return is false)
+      require(addressOfContract.delegatecall.gas(30000)(bytes4(keccak256("helloFunction()"))) == true);
+      
+  }
+
+  /*
+  * @notice A simple invocation that makes sure that the only code run in the fallback
+  * function of the called contract is a logging event. 
+  * @param addressOfContract - The address of the contract to interact with
+  * @param msg.value - The ether the user wants to give to the contract at addressOfContract
+  */
+  function SendInvocation(address addressOfContract) public payable {
+
+      //A send does not propagate exceptions, 
+      //therefore a require function has been added to propagate an exception
+      //if one is found (when the return is false)
+      require(addressOfContract.send(msg.value) == true);
+      
+  }
+
+  /*
+  * @notice a simple invocation that makes sure that the only code run in the fallback
+  * function of the called contract is a logging event. This method propagates errors
+  * @param addressOfContract - The address of the contract to interact with
+  * @param msg.value - The ether the user wants to give to the contract at addressOfContract
+  */
+   function TransferInvocation(address addressOfContract) public payable {
+      
+      addressOfContract.transfer(msg.value);
+      
+  }
+  
+  /*
+  * @notice Calling this function will set this contract to inactive forever.
+  * The remaining ether will be sent to the given address
+  * @param addressOfContract - The address of the contract to interact with
+  */
+  function SelfDestructionInvocation(address addressOfContract) public payable {
+      
+      selfdestruct(addressOfContract);
+      
+  }
+
+}
+
+
+
+
+
+
